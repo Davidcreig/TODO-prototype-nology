277b494bf7401ff91c051ef383afa4adb9aa8ffc
Davidcreig <davidlaptop@outlook.es>
2026-03-25 18:27:22 +0000

    styling

--- src/components/TaskTile/TaskTile.jsx
+++ src/components/TaskTile/TaskTile.jsx
@@ -8,9 +8,11 @@ const TaskTile = ({task, handleDelete}) => {
     const[taskDone, setTaskDone] = useState(false);
 
   return (
-        <div className={styles.tileLayout} onClick={()=> setTaskDone((!taskDone))} >
-            <input checked={taskDone} type='checkbox' id="checkbox" onChange={(e)=> {setTaskDone(!taskDone); console.log(taskDone)}}/>
-            <div className={taskDone ? styles.tileText : ""}>{task}</div>  
+        <div className={styles.tileLayout}>
+            <div className={styles.tileInside} onClick={()=> setTaskDone((!taskDone))} >
+                <input checked={taskDone} type='checkbox' id="checkbox" onChange={(e)=> {setTaskDone(!taskDone);}}/>
+                <div className={taskDone ? styles.tileText : ""}>{task}</div> 
+            </div>  
         <button className={styles.button} onClick={()=>handleDelete(task)}>
             <img className={styles.binIcon} src={deleteIcon} alt="Delete" />
         </button>
